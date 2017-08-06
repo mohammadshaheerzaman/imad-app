d7e61b1440c43495abdead03bf9c7971b55d665f
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-06 15:32:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res){
+    res.send("Article one text will be serverd here.");
+});
+
+app.get('/article-two', function(req,res){
+    res.send("Article two text will be serverd here.");
+});
+
+app.get('/article-three', function(req,res){
+    res.send("Article three text will be serverd here.");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
