def4a4e3fcdb156c7e2f44d70fe8bc4243052867
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-06 18:25:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft += 10;
+    marginLeft += 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function(){
