bde21d5e363cc87580ace12741f5e3109c52aa2c
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-06 18:17:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ var element = document.getElementById('main-text');
 var img = document.getElementById('madi');
 
 img.onclick = function(){
-    img.sytle.marginLeft = '100px';
+    img.style.marginLeft = '100px';
 };
