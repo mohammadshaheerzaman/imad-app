3a3ad022fb83c2ce9f519d9d5fe6a333c90c35af
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 17:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/ui/style.css', function (req, res) {
 var counter = 0;
 app.get('/counter', function(req,res){
  counter = counter +1;
-   res.send(counter);
+   res.send(counter.toString());
     
 });
 
