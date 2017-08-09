73725ae8f357961ab17b0c8cc15411090bfbb69d
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 17:30:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,19 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)  {
+      if(request.status == 200)
+    {
+    var counter = request.responseText;
     var span = document.getElementById('count');
-    
     counter = counter +1;
     span.innerHTML = counter.toString();
+    
+}  
+      
+  }
 };
+    
+   
