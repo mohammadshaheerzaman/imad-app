ca62e3588e2b90d236e30c2e2519a1197bbba392
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 19:19:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ var names = [];
 app.get('/submit-name', function (req, res) {
   var name = req.query.name;
   names.push(name);
-  res.send(JSON.stringigy(names));
+  res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function(req,res){
