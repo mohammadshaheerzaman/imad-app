7ddc2bdf5cccb05dae000c696f5b269308d016a1
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 19:00:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,4 +20,19 @@ button.onclick = function(){
     request.send(null);
 };
     
-   
+//Submit name
+var nameValue = document.getElementById('name');
+var name = nameValue.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+  
+  
+  var names = ['name1', 'name2', 'name3', 'name4'];
+    var ul = '';
+    for(var i=0;i<names.length;i++){
+        ul += '<li>' + names[i] + '</li>';
+    }
+    var unlist = docuemnt.getElementById('unordered');
+    unlist.innerHTML =  ul;
+};
