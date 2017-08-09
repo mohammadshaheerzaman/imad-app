6843126e367236ffcc2ec892d1456c59fa9c3830
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 19:48:55 +0530

    [imad-console] Updates ui/articleone.js

--- ui/articleone.js
+++ ui/articleone.js
@@ -1,1 +1,28 @@
-console.log('Heloo');
+var submit = document.getElementById('commentsubmit');
+submit.onclick = function(){
+  
+  var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)  {
+      if(request.status == 200)
+    {
+    var names = request.responseText;
+    names = JSON.parse(names);
+     var ul = '';
+    for(var i=0;i<names.length;i++){
+        ul += '<li>' + names[i] + '</li>';
+    }
+    var unlist = document.getElementById('unordered');
+    unlist.innerHTML =  ul;
+    
+}  
+      
+  }
+    };
+    var nameValue = document.getElementById('commentid');
+    var name = nameValue.value;
+    request.open('GET','http://shaheermanuu.imad.hasura-app.io/articleName?name='+name,true);
+    request.send(null);
+ };
+
