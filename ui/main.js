632828a6d7e0895e5ebe29ef809a513b5dc29c9f
mohammadshaheerzaman <shaheer.manuu@gmail.com>
2017-08-09 17:33:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ var button = document.getElementById('counter');
 
 button.onclick = function(){
     var request = new XMLHttpRequest();
+    
     request.onreadystatechange = function(){
   if(request.readyState === XMLHttpRequest.DONE)  {
       if(request.status == 200)
@@ -15,6 +16,9 @@ button.onclick = function(){
       
   }
     };
+    
+    request.open('GET','http://shaheermanuu.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
     
    
